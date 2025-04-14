5affb7499c5ff107aca63df9dece52763d9486fa
Federico Luna <federicolun4@gmail.com>
2025-04-13 22:31:07 -0300

    feat: Update Header component structure and add responsive container styles

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,23 +1,26 @@
 import React from "react";
 import Logo from "../ui/logo/logo";
 import "./header.css";
+
 const Header = () => {
   return (
-    <div className="container-header">
-      <Logo />
-      <div className="header-links">
-        <a className="link" href="">
-          Sobre Nosotros
-        </a>
-        <a className="link" href="">
-          Planes
-        </a>
-        <a className="link" href="">
-          Viajes
-        </a>
-        <button className="button">Iniciar sesión</button>
+    <header className="border-b">
+      <div className="container-header container">
+        <Logo />
+        <div className="header-links">
+          <a className="link" href="">
+            Sobre Nosotros
+          </a>
+          <a className="link" href="">
+            Planes
+          </a>
+          <a className="link" href="">
+            Viajes
+          </a>
+          <button className="button">Iniciar sesión</button>
+        </div>
       </div>
-    </div>
+    </header>
   );
 };
 
