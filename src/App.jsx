b327c611358cfba9776685e613a97b31fb05db41
Federico Luna <federicolun4@gmail.com>
2025-04-15 17:18:09 -0300

    feat: Implement layout structure with Header and Footer components, add Trips route, and update Home component

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,20 @@ import Home from "./routes/home/home";
 import NotFound from "./routes/not-found/not-found";
 import Login from "./routes/login/login";
 import Register from "./routes/register/register";
+import Layout from "./components/layout/layout";
+import Trips from "./routes/trips/trips";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/*" element={<NotFound />} />
-        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/trips" element={<Trips />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
