1a59244da1f789d66d2ae7a6b3aadc783de5f0a3
AlvaroReynoso <areynoso@frro.utn.edu.ar>
2025-04-20 19:51:14 -0300

    feat: implement NotFound component with styling and back navigation

--- src/routes/not-found/not-found.jsx
+++ src/routes/not-found/not-found.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { ArrowLeft, Compass } from "phosphor-react";
+import "./not-found.css";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  const handleBackButton = () => {
+    window.history.back();
+  };
+  return (
+    <div>
+      <div className="container-pageNotFound">
+        <img src="../../../public/img-notFound.png" alt="" />
+        <div className="container-notFound">
+          <div className="notFound-title">
+            <h1>4</h1>
+            <Compass size={150} className="notFound-icon" />
+            <h1>4</h1>
+          </div>
+          <div className="notFound-subTitle">
+            <h2>Pagina no encontrada</h2>
+            <p>Parece que estas perdido...deberias volver</p>
+            <button onClick={handleBackButton} className="boton-notFound">
+              Volver <ArrowLeft size={20} />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
