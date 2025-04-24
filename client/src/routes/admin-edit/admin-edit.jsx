29245696f2324b78d40c14cec2cecf532e3e285b
AlvaroReynoso <areynoso@frro.utn.edu.ar>
2025-04-24 04:16:13 -0300

    fix: remove unnecessary whitespace in error message for name validation in Register component

--- client/src/routes/admin-edit/admin-edit.jsx
+++ client/src/routes/admin-edit/admin-edit.jsx
@@ -1,7 +1,8 @@
 import { FloppyDisk } from "phosphor-react";
 import Input from "../../components/ui/input/input";
 import "./admin-edit.css";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { useParams } from "react-router";
 
 const AdminEdit = () => {
   const inputNameRef = useRef(null);
@@ -12,43 +13,129 @@ const AdminEdit = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [role, setRole] = useState("");
-  const [state, setState] = useState("");
+  const [role, setRole] = useState("owner");
+  const [state, setState] = useState("active");
   const [errors, setErrors] = useState({
-    name: false,
-    email: false,
-    password: false,
+    invalidName: false,
+    invalidEmail: false,
+    invalidPassword: false,
     role: false,
     state: false,
   });
+  const [user, setUser] = useState(null);
+  const params = useParams();
+
+  const getUser = (id) => {
+    fetch(`http://localhost:3000/users/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setUser(data);
+        setName(data.name);
+        setEmail(data.email);
+        setPassword(data.password);
+        setRole(data.role);
+        setState(data.status);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    getUser(params.id);
+  }, [params.id]);
+
+  const invalidName = (name) => {
+    return name.length >= 4;
+  };
+
+  const invalidEmail = (email) => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  };
+
+  const invalidPassword = (password) => {
+    return password.length >= 8;
+  };
 
   const handleName = (e) => {
     setName(e.target.value);
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      invalidName: false,
+    }));
   };
 
   const handleEmail = (e) => {
-    const Email = e.target.value;
-    setEmail(Email);
-    setErrors({ ...errors, email: Email.length < 10 });
+    setEmail(e.target.value);
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      invalidEmail: false,
+    }));
   };
 
   const handlePassword = (e) => {
-    const Password = e.target.value;
-    setPassword(Password);
-    setErrors({
-      ...errors,
-      tripStart: Password < new Date().toISOString().split("T")[0],
-    });
+    setPassword(e.target.value);
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      invalidPassword: false,
+    }));
   };
 
   const handleRole = (e) => {
-    const Role = e.target.value;
-    setRole(e.target.checked);
+    setRole(e.target.value);
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      role: false,
+    }));
   };
 
   const handleState = (e) => {
-    const State = e.target.value;
-    setState(e.target.checked);
+    setState(e.target.value);
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      state: false,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors({
+      invalidName: false,
+      invalidEmail: false,
+      invalidPassword: false,
+      role: false,
+      state: false,
+    });
+    let hasError = false;
+
+    if (invalidName(name)) {
+      setErrors((prevErrors) => ({ ...prevErrors, invalidName: true }));
+      if (!hasError) inputNameRef.current.focus();
+      hasError = true;
+    }
+
+    if (invalidEmail(email)) {
+      setErrors((prevErrors) => ({ ...prevErrors, invalidEmail: true }));
+      if (!hasError) inputEmailRef.current.focus();
+      hasError = true;
+    }
+
+    if (invalidPassword(password)) {
+      setErrors((prevErrors) => ({ ...prevErrors, invalidPassword: true }));
+      if (!hasError) inputNameRef.current.focus();
+      hasError = true;
+    }
+
+    if (!role) {
+      setErrors((prevErrors) => ({ ...prevErrors, invalidNane: true }));
+      if (!hasError) inputRoleRef.current.focus();
+      hasError = true;
+    }
+
+    if (!state) {
+      setErrors((prevErrors) => ({ ...prevErrors, state: true }));
+      if (!hasError) inputRoleRef.current.focus();
+      hasError = true;
+    }
   };
 
   return (
@@ -63,98 +150,113 @@ const AdminEdit = () => {
         </div>
         <div className="card">
           <h2>Usuario</h2>
-          <form action="">
-            <div className="container-inputs">
-              <div>
-                <label htmlFor="name">Nombre</label>
-                <Input
-                  placeholder="John Doe"
-                  ref={inputNameRef}
-                  onChange={handleName}
-                  value={name}
-                  id="name"
-                  className={errors.name ? "error" : ""}
-                />
-                <p>Nombre del usuario</p>
-              </div>
-              <div>
-                <label htmlFor="email">Email</label>
-                <Input
-                  placeholder="johndoe@gmail.com"
-                  ref={inputEmailRef}
-                  onChange={handleEmail}
-                  value={email}
-                  id="email"
-                  className={errors.email ? "error" : ""}
-                />
-                <p>Email del usuario</p>
-                {errors.email && (
-                  <p className="error-message">
-                    La descripción debe contener al menos 10 caracteres
-                  </p>
-                )}
-              </div>
-              <div>
-                <label htmlFor="password">Contraseña</label>
-                <Input
-                  placeholder="************"
-                  ref={inputPasswordRef}
-                  onChange={handlePassword}
-                  value={password}
-                  id="password"
-                  className={errors.password ? "error" : ""}
-                />
-                <p>Email del usuario</p>
-                {errors.password && (
-                  <p className="error-message">
-                    La descripción debe contener al menos 10 caracteres
-                  </p>
-                )}
-              </div>
-              <div className="container-rol-status">
-                <label htmlFor="role">Rol</label>
-                <select
-                  ref={inputRoleRef}
-                  placeholder="Ingrese un rol"
-                  id="role"
-                  onChange={handleRole}
-                  value={role}
-                  className="select"
-                >
-                  <option value="owner">Dueño</option>
-                  <option value="admin">Admin</option>
-                  <option value="editor">Editor</option>
-                </select>
+          {user?.name ? (
+            <form action="" onSubmit={handleSubmit}>
+              <div className="container-inputs">
+                <div>
+                  <label htmlFor="name">Nombre</label>
+                  <Input
+                    placeholder="John Doe"
+                    ref={inputNameRef}
+                    onChange={handleName}
+                    value={name}
+                    id="name"
+                    className={errors.invalidName ? "error" : ""}
+                  />
+                  <p>Nombre del usuario</p>
+                  {errors.invalidName && (
+                    <p className="error-message">
+                      El nombre debe tener al menos 4 caracteres
+                    </p>
+                  )}
+                </div>
+                <div>
+                  <label htmlFor="email">Email</label>
+                  <Input
+                    placeholder="johndoe@gmail.com"
+                    ref={inputEmailRef}
+                    onChange={handleEmail}
+                    value={email}
+                    id="email"
+                    className={errors.invalidEmail ? "error" : ""}
+                  />
+                  <p>Email del usuario</p>
+                  {errors.invalidEmail && (
+                    <p className="error-message">El email debe ser valido</p>
+                  )}
+                </div>
+                <div>
+                  <label htmlFor="password">Contraseña</label>
+                  <Input
+                    placeholder="************"
+                    ref={inputPasswordRef}
+                    onChange={handlePassword}
+                    value={password}
+                    id="password"
+                    type="password"
+                    className={errors.invalidPassword ? "error" : ""}
+                  />
+                  <p>Email del usuario</p>
+                  {errors.invalidPassword && (
+                    <p className="error-message">
+                      La contraseña debe tener al menos 8 caracteres
+                    </p>
+                  )}
+                </div>
+                <div className="container-rol-status">
+                  <label htmlFor="role">Rol</label>
+                  <select
+                    ref={inputRoleRef}
+                    placeholder="Ingrese un rol"
+                    id="role"
+                    onChange={handleRole}
+                    value={role}
+                    className={`${errors.role ? "error select" : "select"}`}
+                  >
+                    <option value="owner">Dueño</option>
+                    <option value="admin">Admin</option>
+                    <option value="editor">Editor</option>
+                  </select>
+                  <p>El rol que quieras que tenga el usuario</p>
+                  {errors.role && (
+                    <p className="error-message">Debe elegir un rol</p>
+                  )}
+                </div>
+                <div className="container-rol-status">
+                  <label htmlFor="">Estado</label>
+                  <select
+                    ref={inputStateRef}
+                    placeholder="Ingrese un estado"
+                    id="state"
+                    onChange={handleState}
+                    value={state}
+                    className={`${errors.state ? "error select" : "select"}`}
+                  >
+                    <option value="active">Activo</option>
+                    <option value="blocked">Bloqueado</option>
+                  </select>
 
-                <p>El rol que quieras que tenga el usuario</p>
+                  <p className="description">Estado del usuario</p>
+                  {errors.state && (
+                    <p className="error-message">
+                      El nombre debe tener al menos 4 caracteres
+                    </p>
+                  )}
+                </div>
               </div>
-
-              <div className="container-rol-status">
-                <label htmlFor="">Estado</label>
-                <select
-                  ref={inputStateRef}
-                  placeholder="Ingrese un estado"
-                  id="state"
-                  onChange={handleState}
-                  value={state}
-                  className="select"
-                >
-                  <option value="active">Activo</option>
-                  <option value="blocked">Bloqueado</option>
-                </select>
-
-                <p className="description">Estado del usuario</p>
-              </div>
-            </div>
-
-            <button className="button button-primary button-save">
-              Guardar <FloppyDisk size={20} />
-            </button>
-          </form>
+              <button className="button button-primary button-save">
+                Guardar <FloppyDisk size={20} />
+              </button>
+            </form>
+          ) : (
+            <p>No se encontro el usuario </p>
+          )}
         </div>
       </div>
     </section>
   );
 };
+
+// TODO LUCHO: CAMBIAR TODOS LOS STATE POR STATUS , SOLO LA PALABRA MENOS EN EL USESTATE OBVIO
 
 export default AdminEdit;
