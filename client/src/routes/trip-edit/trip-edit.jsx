f27a10a99acfc7c1102b08c9fce13d919ab45919
Federico Luna <federicolun4@gmail.com>
2025-04-25 20:56:06 -0300

    feat: implement trip editing functionality with TripForm component, update trip services, and fix isBlocked field naming

---
+++ client/src/routes/trip-edit/trip-edit.jsx
@@ -0,0 +1,35 @@
+import TripForm from "../../components/trip-form/trip-form";
+import { Link, useParams } from "react-router";
+import { useEffect } from "react";
+import { useState } from "react";
+import "./trip-edit.css";
+import { ArrowLeft } from "phosphor-react";
+
+const TripEdit = () => {
+  const { id } = useParams();
+  const [initialTrip, setInitialTrip] = useState({});
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/trips/${id}`)
+      .then((res) => res.json())
+      .then((data) => setInitialTrip(data))
+      .catch((error) => console.log(error));
+  }, [id]);
+
+  return (
+    <section className="background">
+      <div className="container trip-edit-container">
+        <div className="trip-button-container">
+          <Link to={`/trip/${id}`}>
+            <button className="button button-outline">
+              Volver <ArrowLeft size={20} />
+            </button>
+          </Link>
+        </div>
+        <TripForm initialTrip={initialTrip} />
+      </div>
+    </section>
+  );
+};
+
+export default TripEdit;
